ad2b600bbc9c6d86143c263d0cfd566ef73d5ce1
cuong <buihoangcuong972@gmail.com>
2021-07-20 23:37:18 +0900

    mainPage変更

--- src/App.js
+++ src/App.js
@@ -102,15 +102,14 @@ import { css } from "styled-components/macro"; //eslint-disable-line
 // import PrivacyPolicyPage from "pages/PrivacyPolicy.js";
 
 import ComponentRenderer from "ComponentRenderer.js";
-import MainLandingPage from "MainLandingPage.js";
 import ThankYouPage from "ThankYouPage.js";
+import MainPage from "demos/RestaurantLandingPage.js";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default function App() {
   // If you want to disable the animation just use the disabled `prop` like below on your page's component
   // return <AnimationRevealPage disabled>xxxxxxxxxx</AnimationRevealPage>;
-
 
   return (
     <Router>
@@ -125,7 +124,7 @@ export default function App() {
           <ThankYouPage />
         </Route>
         <Route path="/">
-          <MainLandingPage />
+          <MainPage />
         </Route>
       </Switch>
     </Router>
